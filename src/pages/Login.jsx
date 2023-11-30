35f22eef38988932e611dc4408309ecbf89ccf08
juliakalvik <julia.kalvik@yahoo.no>
2023-11-30 15:44:14 +0100

    first commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,13 @@
+import "../style/Login.css";
+
+function Login() {
+  return (
+    <>
+      <div>
+        <h1>Log in to your account</h1>
+      </div>
+    </>
+  );
+}
+
+export default Login;
