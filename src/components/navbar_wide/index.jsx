0a9ffdb0c4a02a0eefabf4d22c56fc2f97568af3
juliakalvik <julia.kalvik@yahoo.no>
2023-12-01 13:53:05 +0100

    Added sign up form

--- src/components/navbar_wide/index.jsx
+++ src/components/navbar_wide/index.jsx
@@ -1,16 +1,19 @@
 import { Link } from "@tanstack/react-router";
+import "./navbar.css";
 
 export default function Header() {
   return (
     <>
-      <h1>Header: react hello</h1>
-      <nav className="bar">
+      <nav className="navbar">
         <ul>
           <li>
             <Link to="/">Home</Link>
           </li>
           <li>
             <Link to="/login">Log in</Link>
+          </li>
+          <li>
+            <Link to="/signup">Sign up</Link>
           </li>
         </ul>
       </nav>
---
+++ src/components/signupForm/index.jsx
@@ -0,0 +1,126 @@
+import React, { useState } from "react";
+import "./signup.css";
+
+const SignUpForm = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    avatar: null,
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleAvatarChange = (e) => {
+    const file = e.target.files[0];
+    setFormData((prevData) => ({
+      ...prevData,
+      avatar: file,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const requiredFields = ["name", "email", "password"];
+    const newErrors = {};
+    requiredFields.forEach((field) => {
+      if (!formData[field]) {
+        newErrors[field] = `Please enter ${field}.`;
+      }
+    });
+
+    if (formData.password.length < 8) {
+      newErrors.password = "Password must be at least 8 characters long.";
+    }
+
+    const emailRegex = /^(.+)@(stud\.noroff\.no|noroff\.no)$/;
+    if (!emailRegex.test(formData.email)) {
+      newErrors.email =
+        "Invalid email address. Use stud.noroff.no or noroff.no.";
+    }
+
+    const nameRegex = /^[^\W_]+$/;
+    if (!nameRegex.test(formData.name)) {
+      newErrors.name =
+        "Name should not contain punctuation symbols apart from underscore.";
+    }
+
+    setErrors(newErrors);
+
+    if (Object.keys(newErrors).length > 0) {
+      return;
+    }
+
+    setErrors({
+      name: "",
+      email: "",
+      password: "",
+    });
+
+    console.log("Signup successful:", formData);
+  };
+
+  return (
+    <form className="signup-form" onSubmit={handleSubmit}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+          required
+        />
+        {errors.name && <span className="error">{errors.name}</span>}
+      </label>
+
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+        {errors.email && <span className="error">{errors.email}</span>}
+      </label>
+
+      <label>
+        Password:
+        <input
+          type="password"
+          name="password"
+          value={formData.password}
+          onChange={handleChange}
+          required
+        />
+        {errors.password && <span className="error">{errors.password}</span>}
+      </label>
+
+      <label>
+        Avatar:
+        <input type="file" accept="image/*" onChange={handleAvatarChange} />
+      </label>
+
+      <button className="submit-btn" type="submit">
+        Sign Up
+      </button>
+    </form>
+  );
+};
+
+export default SignUpForm;
