beed4c705efc1d4b600396e586d80c90d74971a2
juliakalvik <julia.kalvik@yahoo.no>
2023-12-07 08:12:47 +0100

    Added hero section to homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import AuctionItems from "../components/auctionItems/index.jsx";
+import HomeHero from "../components/homeHero/index.jsx";
 import Header from "../components/navbar_wide/index.jsx";
 
 export default function HomeTest() {
   return (
     <>
       <Header />
+      <HomeHero />
       <h2>Home page</h2>
       <AuctionItems />
     </>
