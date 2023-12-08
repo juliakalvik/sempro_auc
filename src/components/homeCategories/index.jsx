c581e0ba26683bfc04ee320c0bbb12283166544f
juliakalvik <julia.kalvik@yahoo.no>
2023-12-08 20:49:02 +0100

    Small changes to padding

--- src/components/homeCategories/index.jsx
+++ src/components/homeCategories/index.jsx
@@ -1,6 +1,6 @@
 export default function HomeCategoriesSection() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white py-12">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <h4 className="text-center text-lg font-semibold leading-8 text-gray-900">
           Browse categories:
--- src/components/homeHero/index.jsx
+++ src/components/homeHero/index.jsx
@@ -36,7 +36,7 @@ export default function HomeHeroSection() {
           <div className="mt-10 flex items-center justify-center gap-x-6">
             <a
               href="./signup"
-              className="rounded-md bg-gray-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-turq focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:text-white"
+              className="rounded-md bg-gray-900 px-5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-turq focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 hover:text-white"
             >
               Join here
             </a>
