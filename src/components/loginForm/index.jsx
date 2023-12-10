f23b9cb3f38fc6e93934ab9db1264dab174abece
juliakalvik <julia.kalvik@yahoo.no>
2023-12-10 19:46:46 +0100

    Updated login page

--- src/components/loginForm/index.jsx
+++ src/components/loginForm/index.jsx
@@ -1,6 +1,7 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useNavigate, Link } from "@tanstack/react-router";
 import { loginUser } from "../../lib/api";
+import "../signupForm/signup.css";
 
 const LoginPage = () => {
   const [isLoading, setIsLoading] = useState(false);
@@ -42,40 +43,41 @@ const LoginPage = () => {
   if (error) return <div>An error occurred: {error?.message}</div>;
 
   return (
-    <div className="p-4 rounded-lg dark:bg-gray-900 md:p-10">
-      <div className="flex items-center justify-center">
-        <img
-          className="h-22 sm:h-24 md:h-26 lg:h-28 xl:h-30 my-5 logo dark:invert"
-          alt="Logo"
-        />
+    <div className=" ">
+      <div className="mx-auto max-w-2xl  pt-10">
+        <div className="text-center">
+          <h1 className="lg:text-4xl font-extrabold tracking-tight md:text-4xl text-gray-900 text-3xl">
+            Welcome back to <br></br>
+            <span className="inline-block pl-2 text-7xl transform skew-x-[-18deg]">
+              G
+              <span className="bg-gradient-to-tl from-turq to-black text-transparent bg-clip-text">
+                AV
+              </span>
+              EL
+            </span>{" "}
+          </h1>
+        </div>
       </div>
-      <h1 className="mt-2 mb-5 text-3xl font-bold sm:text-3xl md:text-4xl lg:text-5xl">
-        <p className="text-sm text-white dark:text-gray-700">is</p>Better than
-        X!
-      </h1>
 
-      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-md">
+      <div className="mt-10 mx-auto ">
         {isSuccess ? (
           <section>
             <p className="text-center text-green-900">
-              👋 Hi {data?.name}. You will now redirect to the home page!
+              Hello {data?.name}. Good to see you again!
             </p>
           </section>
         ) : (
           <section>
-            <div className="w-full mt-1 bg-orange-200 border-2 border-orange-100 rounded-3xl md:mt-2 sm:max-w-md xl:p-1 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 sm:space-y-5 md:space-y-7 sm:p-8">
-                <h2 className="font-bold leading-tight tracking-tight text-gray-800 text-l sm:text-xl md:text-2xl lg:text-2xl dark:text-white">
-                  Sign in to your account
+            <div className="signup-form">
+              <div className=" pt-4 pb-4">
+                <h2 className="font-bold leading-tight tracking-tight text-gray-800 text-xl dark:text-white">
+                  Log in to your account
                 </h2>
-                <form
-                  className="p-1 space-y-4 md:space-y-6"
-                  onSubmit={handleOnSubmit}
-                >
+                <form className="p-1 space-y-4" onSubmit={handleOnSubmit}>
                   <div>
                     <label
                       htmlFor="email"
-                      className="block text-sm font-medium leading-6 text-gray-900"
+                      className="block pt-6 text-sm font-medium leading-6 text-gray-900"
                     >
                       Email address
                     </label>
@@ -88,11 +90,18 @@ const LoginPage = () => {
                         placeholder="Email"
                         autoComplete="email"
                         defaultValue="first.last@stud.noroff.no"
-                        className="bg-neutral-100 border-2 border-orange-100 text-gray-900 leading-tight tracking-tight sm:text-sm rounded-3xl focus:ring-primary-600 focus:border-primary-600 block w-full min-w-[220px] sm:min-w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        className="bg-neutral-100 border-2 border-orange-100 text-gray-900 leading-tight tracking-tight text-sm rounded-3xl focus:ring-primary-600 focus:border-primary-600 block w-full min-w-[220px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 "
                       />
                     </div>
                   </div>
+
                   <div className="mt-2">
+                    <label
+                      htmlFor="password"
+                      className="block text-sm font-medium leading-6 text-gray-900"
+                    >
+                      Password
+                    </label>
                     <input
                       id="password"
                       name="password"
@@ -102,7 +111,7 @@ const LoginPage = () => {
                       autoComplete="current-password"
                       minLength={8}
                       defaultValue="UzI1NiIsInR5cCI"
-                      className="bg-neutral-100 border-2 border-orange-100 text-gray-900 leading-tight tracking-tight sm:text-sm rounded-3xl focus:ring-primary-600 focus:border-primary-600 block w-full min-w-[220px] sm:min-w-[300px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="bg-neutral-100 border-2 border-orange-100 text-gray-900 leading-tight tracking-tight text-sm rounded-3xl focus:ring-primary-600 focus:border-primary-600 block w-full min-w-[220px] p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     />
                   </div>
                   <div className="flex items-center justify-between">
@@ -115,35 +124,29 @@ const LoginPage = () => {
                           className="w-4 h-4 ml-2 border border-gray-100 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
                         />
                       </div>
-                      <div className="ml-1 text-sm">
+                      <div className="ml-1 text-sm ">
                         <label
                           htmlFor="remember"
-                          className="text-xs font-light text-gray-600 dark:text-gray-300 sm:text-xs md:text-xs lg:text-sm"
+                          className="text-xs font-light text-turq dark:text-turq"
                         >
                           Remember me
                         </label>
                       </div>
                     </div>
-                    <a
-                      href="#"
-                      className="ml-1 text-xs font-medium text-gray-700 dark:text-gray-400 sm:text-sm md:text-sm lg:text-sm text-primary-600 hover:underline"
-                    >
-                      Forgot password?
-                    </a>
                   </div>
                   <div>
                     <button
                       disabled={isLoading}
                       type="submit"
-                      className="w-full px-4 py-2 my-2 leading-tight tracking-tight text-white bg-blue-500 border-2 border-blue-500 rounded-3xl hover:border-blue-400 shadow-custom"
+                      className="w-full px-4 py-2 my-2 leading-tight tracking-tight text-white "
                     >
                       {isLoading ? "Signing In" : "Login"}
                     </button>
                     <p className="text-xs font-light text-gray-700 sm:text-sm dark:text-gray-400">
                       Not a member?{" "}
                       <Link
-                        to="/"
-                        className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+                        to="/signup"
+                        className="font-semibold leading-6 text-turq hover:text-gray-800"
                       >
                         Sign up now
                       </Link>
