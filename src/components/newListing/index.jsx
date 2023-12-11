7715bb574cb8f7373393b6fb2b38ead603628796
juliakalvik <julia.kalvik@yahoo.no>
2023-12-11 20:07:17 +0100

    Small changes to listing form

--- src/components/newListing/index.jsx
+++ src/components/newListing/index.jsx
@@ -10,8 +10,21 @@ export default function NewListing() {
     setSelectedDate(date);
   };
   return (
-    <form>
-      <div className="space-y-12">
+    <form className="p-5">
+      <div
+        className="pt-32  flex justify-end ml-10 absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tl from-turq to-yellow opacity-70 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 99.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
+      </div>
+
+      <div className="space-y-12 text-left">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
             New Listing
@@ -20,23 +33,22 @@ export default function NewListing() {
             This information will be displayed publicly.
           </p>
 
-          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 ">
             <div className="sm:col-span-4">
               <label
-                htmlFor="username"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                htmlFor="title"
+                className="block text-sm font-medium leading-6  text-gray-900"
               >
                 Listing title
               </label>
               <div className="mt-2">
-                <div className=" flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <div className="border-2 flex rounded-md ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md ">
                   <input
                     type="text"
                     placeholder="Title"
-                    name="username"
-                    id="username"
-                    autoComplete="username"
-                    className=" block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                    name="listingTitle"
+                    id="listingTitle"
+                    className="bg-white block flex-1 rounded-md border-0  bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
                   />
                 </div>
                 <p className="mt-3 text-sm leading-6 text-gray-600">
@@ -59,7 +71,7 @@ export default function NewListing() {
                   name="about"
                   rows={3}
                   placeholder="Description"
-                  className="p-3 block w-full lg:max-w-md md:max-w-md rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                  className="p-3 block w-full lg:max-w-md md:max-w-md rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset  sm:text-sm sm:leading-6"
                   defaultValue={""}
                 />
               </div>
@@ -71,7 +83,7 @@ export default function NewListing() {
               <div className="p-2">
                 <label
                   htmlFor="deadline"
-                  className="block text-sm font-medium leading-6 text-gray-900"
+                  className=" block text-sm font-medium leading-6 text-gray-900"
                 >
                   Deadline
                 </label>
@@ -135,7 +147,7 @@ export default function NewListing() {
                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
                       <label
                         htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-turq hover:text-gray-800"
                       >
                         <span>Upload a file</span>
                         <input
@@ -159,7 +171,7 @@ export default function NewListing() {
                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
                       <label
                         htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-turq hover:text-gray-800"
                       >
                         <span>Upload a file</span>
                         <input
@@ -183,7 +195,7 @@ export default function NewListing() {
                     <div className="mt-4 flex text-sm leading-6 text-gray-600">
                       <label
                         htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-turq hover:text-gray-800"
                       >
                         <span>Upload a file</span>
                         <input
@@ -205,7 +217,7 @@ export default function NewListing() {
           </div>
         </div>
 
-        <div className="border-b border-gray-900/10 pb-12">
+        <div className=" border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
             Your listing is ready!
           </h2>
@@ -218,16 +230,16 @@ export default function NewListing() {
         </div>
       </div>
 
-      <div className="mt-6 flex items-center justify-end gap-x-6">
+      <div className="mt-2 flex gap-x-6">
         <button
           type="button"
-          className="text-sm font-semibold leading-6 text-gray-900"
+          className="text-sm font-semibold hover:border-turq leading-6 text-gray-900"
         >
           Cancel
         </button>
         <button
           type="submit"
-          className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          className="rounded-md bg-turq px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-gray-800 hover:border-gray-800"
         >
           Save
         </button>
