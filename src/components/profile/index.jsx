b3b62cdff8e39e5118503447429b3d67c759bd69
juliakalvik <julia.kalvik@yahoo.no>
2023-12-15 18:44:00 +0100

    Updated UI on profile page

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -32,8 +32,9 @@ const Profile = () => {
       console.error("Error fetching listing details:", error);
     }
   };
+
   const handleAvatarChange = async () => {
-    if (imageURL == "") return;
+    if (imageURL === "") return;
     const data = await putUpdateEntryMedia(
       localStorage.getItem("user_name"),
       imageURL
@@ -54,38 +55,46 @@ const Profile = () => {
     setIsModalOpen(false);
   };
 
+  const isBidActive = (endTime) => {
+    return new Date() < new Date(endTime);
+  };
+
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="max-w-md w-full md:flex md:flex-col md:items-start">
-        {/* Avatar Section */}
+    <div className="flex flex-col lg:flex-row mt-8 text-left lg:items-start">
+      {/* Left Side */}
+      <div className="lg:w-1/3 lg:pr-8 mb-8 lg:mb-0">
         <Card>
-          <label
-            htmlFor="avatar"
-            className="block text-sm font-medium text-gray-800"
-          >
-            Profile Photo
-          </label>
-          <div className="mt-1 flex items-center justify-center">
+          <div className="mt-1 flex items-center justify-center text-center">
             <div className="relative">
+              <p className="text-lg font-semibold pb-5">{profile?.name}</p>
               <img
                 src={avatarUrl}
                 alt="Profile"
-                className="w-24 h-24 rounded-full mx-auto mb-2 cursor-pointer"
+                className="w-48 h-48 object-cover rounded-full mx-auto mb-2 cursor-pointer"
                 onClick={openModal}
               />
-
-              <button
-                className="block bg-gray-800 hover:bg-turq text-white py-2 px-4 rounded-md cursor-pointer"
-                onClick={() => handleAvatarChange()} // Trigger file input
-              >
-                Change Photo
-              </button>
-              <input type="url" value={imageURL} onChange={handleInputChange} />
+              <p className="text-sm font-semibold py-3">
+                Use an URL to change your profile photo:
+              </p>
+              <div className="flex ml-2">
+                <input
+                  type="url"
+                  value={imageURL}
+                  onChange={handleInputChange}
+                  className="p-2 border border-gray-300 rounded-md"
+                  placeholder="Image URL"
+                />
+                <button
+                  className="ml-2 bg-gray-800 hover:bg-turq text-white py-2 px-4 rounded-md cursor-pointer"
+                  onClick={() => handleAvatarChange()} // Trigger file input
+                >
+                  Update
+                </button>
+              </div>
             </div>
           </div>
         </Card>
 
-        {/* Overview Section */}
         <Card>
           <h2 className="text-2xl font-bold text-gray-800 mb-4">Overview</h2>
           <div className="flex items-center justify-between mb-2">
@@ -99,40 +108,65 @@ const Profile = () => {
             </span>
           </div>
         </Card>
-
-        {/* Additional Section on the Right */}
-        <Card>
-          <p className="text-gray-800">
-            Fetch a list of the users listings here
-          </p>
-          <p>hwlluu</p>
-          {profile?.listings?.map((item) => {
-            return (
-              <>
-                <hr></hr>
-                <br></br>
-                <h4>{item?.title}</h4>
-                <p>{item?.description}</p>
-                <CountdownTimer endsAt={item?.endsAt} />
-              </>
-            );
-          })}
-        </Card>
-
-        {/* Modal */}
-        <Modal
-          isOpen={isModalOpen}
-          onRequestClose={closeModal}
-          contentLabel="Profile Photo"
-        >
-          <img
-            src={avatarUrl}
-            alt="Profile"
-            className="w-full h-full object-contain"
-          />
-          <button onClick={closeModal}>Close</button>
-        </Modal>
       </div>
+
+      {/* Right Side */}
+      <div className="lg:w-2/3">
+        <Card>
+          <p className="text-gray-800">Your listings</p>
+          <div className="flex justify-center">
+            <table className="w-full border-t border-b border-gray-300">
+              <thead className="text-turq">
+                <tr>
+                  <th className="p-3 font-bold">Title</th>
+                  <th className="p-3 font-bold hidden sm:block">Description</th>
+                  <th className="p-3 font-bold">Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {profile?.listings?.map((item, index) => (
+                  <tr
+                    key={item.id}
+                    className={`border-b border-gray-300 ${
+                      index % 2 === 0 ? "bg-gray-100" : "bg-white"
+                    } hover:bg-gray-200 transition`}
+                  >
+                    <td className="p-3 font-semibold">{item?.title}</td>
+                    {/* Hide on small screens, show on screens larger than or equal to small (sm) */}
+                    <td className="p-3 hidden sm:block">{item?.description}</td>
+                    <td className="p-3 font-semibold">
+                      {isBidActive(item.endsAt) ? (
+                        <span className="text-green-600">Active</span>
+                      ) : (
+                        <span className="text-red-600">Inactive</span>
+                      )}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </Card>
+      </div>
+
+      {/* Modal */}
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Profile Photo"
+      >
+        <img
+          src={avatarUrl}
+          alt="Profile"
+          className="w-full h-full object-contain"
+        />
+        <button
+          onClick={closeModal}
+          className="absolute top-2 right-2 bg-gray-800 text-white py-2 px-4 rounded-md cursor-pointer"
+        >
+          Close
+        </button>
+      </Modal>
     </div>
   );
 };
